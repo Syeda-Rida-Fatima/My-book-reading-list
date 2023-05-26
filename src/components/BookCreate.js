901d5b12c815cfb22ddd403a96a9e460de64a2ec
Rida Fatima <syedaridafatima67@gmail.com>
2023-05-26 16:18:53 +0500

    my first commit

---
+++ src/components/BookCreate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BookCreate(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default BookCreate;
---
+++ src/components/BookEdit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BookEdit(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default BookEdit;
