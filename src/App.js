ba717190fd4654b175dfc0f985f4feeaa290222d
Rida Fatima <syedaridafatima67@gmail.com>
2023-05-27 13:35:20 +0500

    Add bookList prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { useState } from 'react';
-import './App.css';
+import BookList from './components/BookList'
+
+import BookCreate from './components/BookCreate';
 
 function App() {
 
-  const [bookx, setBooks] = useState([])
+  const [books, setBooks] = useState([])
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books, {
+        id: Math.round(Math.random() * 9999),
+        title,
+      },
+    ];
+  }
   return (
-   
+    <div>
+      <BookList books={books} />
+      <BookCreate onCreate={createBook} />
+    </div>
   );
 }
 
